42a946aa860acbe8eafccb2b0bdfd08b5c5a5efc
Reksio Croft <octavian.staicu@s.unibuc.ro>
2021-07-07 00:10:54 +0300

    Initial commit

---
+++ android/app/src/main/java/ro/tav/flutter_google_atelier_digital/flutter_google_atelier_digital/MainActivity.java
@@ -0,0 +1,6 @@
+package ro.tav.flutter_google_atelier_digital.flutter_google_atelier_digital;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
